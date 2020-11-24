4c85a6fae95deaa117887bb136665df43e3b158b
Joe H. Rahme <jhakimra@redhat.com>
2020-11-24 22:06:48 +0100

    foo

--- script.js
+++ script.js
@@ -82,3 +82,4 @@ changeage(joe);           // The function "changeage" is called and the variable
 
 
 console.log(joe.age);   // The property of the Object Joe is now changed and JS will log here the new value of 1000000.
+foo
